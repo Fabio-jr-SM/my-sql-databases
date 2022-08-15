5c1477e10277a8162ab99a8056a4eea720924c5c
Fabio Junior <91484736+Fabio-jr-SM@users.noreply.github.com>
2022-08-15 15:13:35 -0400

    Delete Livraria directory

--- Livraria/Authors.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `teste`.`authors` (
-  `AuthorID` INT NOT NULL,
-  `FisrtName` VARCHAR(45) NULL,
-  `LastName` VARCHAR(45) NULL,
-  PRIMARY KEY (`AuthorID`)
-  );
--- Livraria/titles.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `teste`.`titles` (
-  `ISBN` INT NOT NULL,
-  `Title` VARCHAR(45) NULL,
-  `EditionNumber` INT NULL,
-  `CopyRight` VARCHAR(45) NULL,
-  PRIMARY KEY (`ISBN`)
-  );
